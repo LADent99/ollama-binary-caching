519c4889b130ba564dec43eb35a4951e9e728098
Lucas Denton <ladent99@gmail.com>
2026-04-08 09:16:54 -0400

    needed inherit

--- flake.nix
+++ flake.nix
@@ -10,6 +10,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
+            inherit system;
             config.allowUnfree = true;
         };
       in {
